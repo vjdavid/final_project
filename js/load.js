9fb4fca99b3ec543cd7e6c81729ae341719787a2
David <vjdavid621@gmail.com>
2020-11-10 11:35:47 -0800

    movierator branding

--- js/load.js
+++ js/load.js
@@ -1,5 +1,10 @@
 angular.module('moviesApp', [])
-  .controller('SearchController', function($scope) {
+  .controller('SearchController', function($scope, $http) {
+
+    $http.get('https://api.themoviedb.org/3/movie/top_rated?api_key=97d131009d6fad412fe943c626f285d0&language=es-MX&page=1').then(function (response) {
+      console.log('te la pelaste paps');
+      console.log(response);
+    });
 
     $scope.Movies = [
       { 
@@ -9,7 +14,7 @@ angular.module('moviesApp', [])
         descripcion: "Los ganadores del Premio de la Academia® Matt Damon y Christian Bale protagonizan CONTRA LO IMPOSIBLE, la extraordinaria historia verdadera del visionario diseñador de autos estadounidense Carroll Shelby (Damon) y el intrépido conductor británico Ken Miles (Bale), quienes lucharon juntos contra la imposición corporativa, las leyes de la física y sus demonios personales, para construir un coche de carreras revolucionario para Ford Motor Company y así enfrentar a los dominantes autos de carrera de Enzo Ferrari en las 24 Horas de Le Mans en Francia en 1966.",
         duracion: "153 min",
         genero: "Accion",
-        horario: "18:00" 
+        calificacion: "8.5"
       },
       { 
         titulo: "Star Wars: El Ascenso de Skywalker", 
@@ -18,7 +23,7 @@ angular.module('moviesApp', [])
         descripcion: "Lucasfilm y el director J.J. Abrams unen sus fuerzas una vez más para llevar a los espectadores a un viaje épico con STAR WARS: EL ASCENSO DE SKYWALKER. En esta fascinante conclusión de la saga Skywalker, nacerán nuevas leyendas y se librará la batalla final por la libertad.",
         duracion: "155 min",
         genero: "Accion",
-        horario: "22:00" 
+        calificacion: "8.0" 
       },
       { 
         titulo: "Placa de Acero", 
@@ -27,7 +32,7 @@ angular.module('moviesApp', [])
         descripcion: "Roberto S. Recto (Alfonso Dosal), oficial ejemplar recién egresado de la academia se une a la fuerza de policía y es asignado con su nuevo compañero, Adrián Vázquez, patrullero corrupto y holgazán que ha cedido su alma a la corrupción por creer que no existe otro camino posible en este sistema. Juntos tendrán que ir aprendiendo uno del otro para lograr tener la fuerza necesaria para triunfar sobre el crimen organizado y así traer justicia y salvación a la Ciudad de México.",
         duracion: "102 min",
         genero: "Comedia",
-        horario: "13:00" 
+        calificacion: "4.9"
       },
       { 
         titulo: "Frozen 2", 
@@ -36,7 +41,7 @@ angular.module('moviesApp', [])
         descripcion: "¿Por qué Elsa nació con poderes mágicos? La respuesta la está llamando y amenazando su reino. Junto con Anna, Kristoff, Olaf y Sven, se embarcará en un peligroso, pero extraordinario viaje. En FROZEN: UNA AVENTURA CONGELADA, Elsa temía que sus poderes fueran demasiado para el mundo. En FROZEN 2, ella debe esperar que sean suficientes.",
         duracion: "101 min",
         genero: "Animacion",
-        horario: "16:00" 
+        calificacion: "6.6"
       },
       { 
         titulo: "Midway: Batalla en el Pacifico", 
@@ -45,7 +50,7 @@ angular.module('moviesApp', [])
         descripcion: "Mientras la Segunda Guerra Mundial avanzaba, un choque entre la flota estadounidense y la Armada Imperial Japonesa que marcó un punto histórico en el mundo. MIDWAY Guerra en el Pacífico narra la historia real de los dirigentes y soldados que se encontraron en ese momento, demostrando sus hazañas heroicas, instintos de supervivencia, fortaleza y valentía para superar las dificultades de lo que fue uno de los más grandes eventos para toda la humanidad.",
         duracion: "138 min",
         genero: "Accion",
-        horario: "17:00" 
+        calificacion: "4.4"
       },
       { 
         titulo: "Reflejos Siniestros", 
@@ -54,7 +59,7 @@ angular.module('moviesApp', [])
         descripcion: "Los jóvenes de un internado encuentran en el ala abandonada del edificio un misterioso espejo frente al cual piden deseos, lo que descubrirán es que el espejo le pertenecía a la Condesa Obolenskaya, y su fantasma está dispuesto a cumplir sus deseos a cambio de sus almas.",
         duracion: "84 min",
         genero: "Terror",
-        horario: "19:00" 
+        calificacion: "5.0" 
       },
       { 
         titulo: "Los Muertos no Mueren", 
@@ -63,7 +68,7 @@ angular.module('moviesApp', [])
         descripcion: "En la apacible localidad de Centerville pasa algo raro. Los animales han comenzado a comportarse de manera extraña. Nadie sabe por qué y las noticias son desconcertantes. A pesar de todo, nadie es capaz de prever la mayor invasión que pronto comenzará a sacudir Centerville: los muertos ya no están muertos. Se alzan sobre sus tumbas y atacan salvajemente a los vivos para devorarlos; y los lugareños, que hasta el momento creían vivir una vida apacible, se ven obligados a luchar para sobrevivir. El director y guionista Jim Jarmusch (Paterson, Solo los amantes sobreviven), regresa con una aterradora comedia y con el mejor reparto zombie de la historia: Bill Murray, Adam Driver, Tilda Swinton, Selena Gómez, Chloë Sevigny, Iggy Pop y más.",
         duracion: "105 min",
         genero: "Terror",
-        horario: "20:00" 
+        calificacion: "5.8" 
       },
       { 
         titulo: "Doctor Sueño", 
@@ -72,7 +77,7 @@ angular.module('moviesApp', [])
         descripcion: "Doctor Sueño retoma la historia de Danny Torrance 40 años después de su escalofriante estancia en el hotel Overlook en El Resplandor. Ewan McGregor, Rebecca Ferguson y la actriz revelación Kyliegh Curran protagonizan este thriller sobrenatural dirigido por Mike Flanagan, quien creó el guion basado en la novela de Stephen King.",
         duracion: "152 min",
         genero: "Terror",
-        horario: "19:00" 
+        calificacion: "4.5" 
       },
       { 
         titulo: "Jugando con Fuego", 
@@ -81,7 +86,7 @@ angular.module('moviesApp', [])
         descripcion: "Doctor Sueño retoma la historia de Danny Torrance 40 años después de su escalofriante estancia en el hotel Overlook en El Resplandor. Ewan McGregor, Rebecca Ferguson y la actriz revelación Kyliegh Curran protagonizan este thriller sobrenatural dirigido por Mike Flanagan, quien creó el guion basado en la novela de Stephen King.",
         duracion: "96 min",
         genero: "Comedia",
-        horario: "18:00" 
+        calificacion: "5.9" 
       }
     ];
 });
